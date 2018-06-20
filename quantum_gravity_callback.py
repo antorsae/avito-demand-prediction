fb9cd6790458483b345bc182ef428abe46cab70e
pavelgonchar <pavel.gonchar@gmail.com>
2018-06-20 13:47:25 -0700

    fixed bug when there are no peaks

--- quantum_gravity_callback.py
+++ quantum_gravity_callback.py
@@ -3,6 +3,7 @@ from keras.callbacks import *
 from keras.utils.data_utils import get_file
 import numpy as np
 
+
 class QuantumGravityCallback(Callback):
     def __init__(self):
         super(QuantumGravityCallback, self).__init__()
@@ -17,40 +18,37 @@ class QuantumGravityCallback(Callback):
         self.eps = 1e-10
 
     def on_batch_end(self, batch, logs=None):
-        #print(logs['rmse_old'])
 
         y = np.zeros_like(logs['y'])
         for i in range(y.shape[0]):
-            #print(i, logs['x'][2][i], logs['y'][i])
             peaks, weights = self.peaks_map[self.tknz[logs['x'][2][i]]]
-            idx = np.argwhere(peaks <= logs['y'][i])[-1][-0]
+            if len(peaks) == 0:
+                y[i] = logs['y'][i]
+                continue
+            idx = np.argwhere(peaks <= logs['y'][i])[-1][0]
+
             if len(peaks) == idx + 1:
                 logs['y'][i] = peaks[-1]
             else:
                 d1 = peaks[idx] - logs['y'][i] + self.eps
-                d2 = peaks[idx+1]-logs['y'][i] + self.eps
-                w2 = weights[idx+1] / d2
+                d2 = peaks[idx + 1] - logs['y'][i] + self.eps
+                w2 = weights[idx + 1] / d2
                 w1 = weights[idx] / d1
 
-                #print("was", logs['y'][i])
                 if w2 > w1:
-                    logs['y'][i] = peaks[idx+1]
+                    y[i] = peaks[idx + 1]
                 else:
-                    logs['y'][i] = peaks[idx]
-                #print("become", logs['y'][i])
+                    y[i] = peaks[idx]
 
         output = self.model.evaluate(
-                            logs['x'], y,
-                            batch_size=y.shape[0],
-                            sample_weight=None,
-                            verbose=0)
-        print(' Quantum Gravity Losses: %f, %f' % (output[0], output[1]), end='')
+            logs['x'], y, batch_size=y.shape[0], sample_weight=None, verbose=0)
+        print(' Quantum Gravity Losses: %f, %f' % (output[0], output[1]))
         #assert False
         pass
+
 
 if False:
     gravity_callback = QuantumGravityCallback()
     print(gravity_callback.tknz)
     test_array = np.array([0.0, 10.0, 9.0], dtype=np.float32)
     print(np.vectorize(lambda x: gravity_callback.tknz[int(x)])(test_array))
-
